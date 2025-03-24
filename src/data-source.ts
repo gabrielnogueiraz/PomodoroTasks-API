817879b104d38493896d8448d9c7cffc9ee4e7e3
gabrielnogueiraz <gabriel.nogueira00810@gmail.com>
2025-03-24 17:18:54 -0300

    adiciona configuração de fonte de dados com TypeORM e SQLite

---
+++ src/data-source.ts
@@ -0,0 +1,17 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { Task } from "./entities/Task";
+import { Pomodoro } from "./entities/Pomodoro";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: "sqlite",
+  database: process.env.DATABASE_PATH || "src/database/database.sqlite",
+  synchronize: true,
+  logging: false,
+  entities: [Task, Pomodoro],
+  migrations: [],
+  subscribers: [],
+});
