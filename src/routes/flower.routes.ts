eec54e95395d2988657c596dfaeac40c879ea7c2
gabrielnogueiraz <gabriel.nogueira00810@gmail.com>
2025-05-30 13:51:29 -0300

    Adiciona rotas para gerenciamento de flores e jardim do usuário

---
+++ src/routes/flower.routes.ts
@@ -0,0 +1,30 @@
+import { Router, Request, Response } from "express";
+import { FlowerController } from "../controllers/FlowerController";
+import { authMiddleware } from "../middleware/auth";
+
+const flowerRoutes = Router();
+const flowerController = new FlowerController();
+
+flowerRoutes.use(authMiddleware);
+
+flowerRoutes.get("/", (req: Request, res: Response) =>
+  flowerController.getUserFlowers(req, res)
+);
+
+flowerRoutes.get("/garden", (req: Request, res: Response) =>
+  flowerController.getUserGarden(req, res)
+);
+
+flowerRoutes.get("/stats", (req: Request, res: Response) =>
+  flowerController.getGardenStats(req, res)
+);
+
+flowerRoutes.get("/check-rare", (req: Request, res: Response) =>
+  flowerController.checkRareFlower(req, res)
+);
+
+flowerRoutes.post("/", (req: Request, res: Response) =>
+  flowerController.createFlowerForPomodoro(req, res)
+);
+
+export { flowerRoutes };
