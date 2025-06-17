17b5044a24f6fb3c3cf0218dc44dd15cabf10827
gabrielnogueiraz <gabriel.nogueira00810@gmail.com>
2025-06-17 16:44:42 -0300

    feat: adiciona entidade KanbanBoard com propriedades e relacionamentos

---
+++ src/entities/KanbanBoard.ts
@@ -0,0 +1,48 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToOne,
+  OneToMany,
+  JoinColumn,
+  ManyToOne,
+} from "typeorm";
+import { Goal } from "./Goal";
+import { User } from "./User";
+import { KanbanColumn } from "./KanbanColumn";
+
+@Entity()
+export class KanbanBoard {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ nullable: true })
+  description: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @OneToOne(() => Goal, (goal) => goal.kanbanBoard)
+  @JoinColumn()
+  goal: Goal;
+
+  @ManyToOne(() => User, (user) => user.kanbanBoards)
+  user: User;
+
+  @OneToMany(() => KanbanColumn, (column) => column.board, {
+    cascade: true,
+    eager: true,
+  })
+  columns: KanbanColumn[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
